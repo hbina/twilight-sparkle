a12dbdb74b8fd251d467902ec0729f7b0c91e9e6
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2021-02-20 16:47:07 +0800

    v0.2.0 Publishing to NPM!
    Also add ability to parse YAML files.
    
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- src/parsers/json.rs
+++ src/parsers/json.rs
@@ -27,13 +27,13 @@ impl std::convert::From<std::num::ParseIntError> for JsonSolverError {
 impl JsonSolver {
     fn resolve_json_value_with_expression<'a>(
         value: &'a serde_json::Value,
-        expression: &str,
+        expression: &Vec<&str>,
     ) -> Result<&'a serde_json::Value, JsonSolverError> {
         let mut reader = value;
-        for expr in expression.split('.') {
+        for expr in expression {
             match reader {
                 serde_json::Value::Object(map) => {
-                    if let Some(value) = map.get(expr) {
+                    if let Some(value) = map.get(*expr) {
                         reader = value;
                     } else {
                         return Err(JsonSolverError::KeyNotExist(
@@ -62,26 +62,23 @@ impl JsonSolver {
 
     fn json_value_to_string(value: &serde_json::Value) -> String {
         match value {
-            serde_json::Value::Null => "null".to_string(),
-            serde_json::Value::Bool(b) => {
-                if *b {
-                    "true".to_string()
-                } else {
-                    "false".to_string()
-                }
-            }
-            serde_json::Value::Number(n) => n.to_string(),
             serde_json::Value::String(s) => s.clone(),
-            serde_json::Value::Array(arr) => format!("{:?}", arr),
             o => serde_json::to_string(o).unwrap(),
         }
     }
+}
 
-    pub fn solve(input: &String, expression: &str) -> String {
+impl crate::Solver for JsonSolver {
+    fn solve(input: &str, expression: &str) -> String {
         let json_value = serde_json::from_str::<serde_json::Value>(&input)
             .expect("Cannot parse input as a JSON file");
+        let expression = if expression.is_empty() {
+            vec![]
+        } else {
+            expression.split('.').collect()
+        };
         let resolved_value =
-            JsonSolver::resolve_json_value_with_expression(&json_value, expression).unwrap();
+            JsonSolver::resolve_json_value_with_expression(&json_value, &expression).unwrap();
         JsonSolver::json_value_to_string(resolved_value)
     }
 }
