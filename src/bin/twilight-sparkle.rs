cc2ad18898a168f6fcad3ccda276de39ff706002
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2021-03-07 16:58:01 +0800

    Adding basic Gitlab CI/CD
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- src/bin/twilight-sparkle.rs
+++ src/bin/twilight-sparkle.rs
@@ -6,43 +6,40 @@ fn main() {
         .author(clap::crate_authors!())
         .about(clap::crate_description!())
         .arg(
-            clap::Arg::with_name("input")
-                .short("i")
-                .long("input")
-                .help("Input file. If not specified, will read from stdin")
+            clap::Arg::with_name("file")
+                .long("file")
+                .help("Input file. If not specified, will read from stdin.")
                 .takes_value(true),
         )
         .arg(
-            clap::Arg::with_name("output")
-                .short("o")
-                .long("output")
-                .help("Output file. If not specified, will write to stdout")
+            clap::Arg::with_name("write")
+                .long("write")
+                .help("Output file. If not specified, will write to stdout.")
                 .takes_value(true),
         )
         .arg(
-            clap::Arg::with_name("file_type")
-                .short("t")
-                .long("type")
-                .help("What to interpret the input as")
+            clap::Arg::with_name("file-type")
+                .long("file-type")
+                .help(
+                    r##"What to interpret the input as. This is usually helpful if using stdin because we only infer the type from the extension."##,
+                )
                 .takes_value(true)
                 .required(true),
         )
         .arg(
             clap::Arg::with_name("expression")
-                .short("e")
                 .long("expression")
-                .help("Expression to evaluate the input with")
+                .help("Expression to evaluate the input with.")
                 .takes_value(true),
         )
         .arg(
             clap::Arg::with_name("replace")
-                .short("r")
                 .long("replace")
-                .help("Value to replace it with")
+                .help("Value to replace the value resolved by the expression with.")
                 .takes_value(true),
         )
         .get_matches();
-    let input = if let Some(path) = matches.value_of("input") {
+    let input = if let Some(path) = matches.value_of("file") {
         std::fs::read_to_string(path).unwrap()
     } else {
         let mut input = String::new();
@@ -51,14 +48,15 @@ fn main() {
     };
     let expression = matches.value_of("expression");
     let replace = matches.value_of("replace");
-    let file_type = matches.value_of("file_type").unwrap();
+    let file_type = matches.value_of("file-type").unwrap();
     let result = parsers::solve(file_type, &input, expression, replace);
-    if let Some(output) = matches.value_of("output") {
+    if let Some(output) = matches.value_of("write") {
         let mut file = std::fs::OpenOptions::new()
             .read(true)
             .write(true)
             .create(true)
             .truncate(true)
+            .append(false)
             .open(output)
             .unwrap();
         std::io::Write::write_all(&mut file, result.as_bytes()).unwrap();
