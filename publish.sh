548b9860b3f0618149b5d4630630cf28eff22b7f
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2021-08-01 16:14:16 +0800

    Renaming the project and removing wonky nodejs support
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- publish.sh
+++
@@ -1,4 +0,0 @@
-CARGO_VERSION="v$(cat Cargo.toml | twilight-sparkle --type TOML --expression "package.version")"
-echo $CARGO_VERSION
-git tag $CARGO_VERSION
-git push origin $CARGO_VERSION
--- update_version.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-CARGO_VERSION=$(cat Cargo.toml | twilight-sparkle --type TOML --expression "package.version")
-twilight-sparkle --input package.json --type JSON --expression "version" --replace "\"$CARGO_VERSION\"" --output package.json
-npx prettier --write package.json --loglevel silent
